872926dc4cc84dfd698f6f2759d3d0c59bbf72e1
Shvyrkov Yuri <mailto:shvyrkov@yandex.ru>
2022-06-16 19:07:21 +0300

    'Business_logic-,lk'

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controllers;
 
-use App\View\View;
+use \SplFileInfo;
 use App\Components\Menu;
-use App\View\LoginView;
+use App\Components\SimpleImage;
+use App\Model\Users;
+use App\Model\Roles;
+use App\View\View;
+// use App\View\LoginView;
 use App\View\RegistrationView;
-use App\View\LkView;
+// use App\View\LkView;
 use App\View\PasswordView;
 use App\View\UnsubscribeView;
 use App\View\SubscriptionView;
@@ -24,7 +28,38 @@ class UserController
      */
     public function login()
     {
-        return new LoginView('login', ['title' => Menu::showTitle(Menu::getUserMenu())]); // Вывод представления
+        if (isset($_POST['submit'])) { // Обработка формы авторизации
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            // Валидация полей
+            if (!Users::checkEmail($email)) {
+                $errors['email'] = 'Некорректный email';
+            } else {
+                $user = Users::checkUserData($email, $password);
+
+                if ($user) {
+                    // Если данные правильные, запоминаем пользователя (сессия)
+                    Users::auth($user);
+                    // Перенаправляем пользователя в закрытую часть - кабинет 
+                    header('Location: /lk');
+                } else {
+                    // Если данные неправильные - показываем ошибку
+                    $errors['wrongData'] = 'Неправильные данные для входа.<br>
+                                Возможно нажата клавиша CapsLock или несоответствующая раскладка клавиатуры';
+                }
+            }
+        }
+
+        return new View(
+            'login',
+            [
+                'title' => 'Вход',
+                'email' =>  $email ?? '',
+                'password' => $password ?? '',
+                'errors' =>  $errors ?? '',
+            ]
+        );
     }
 
     /**
@@ -72,13 +107,137 @@ class UserController
     /**
      * Вывод страницы личного кабинета пользователя
      *
-     * @return LkView - объект представления страницы личного кабинета пользователя
+     * @return View - объект представления страницы личного кабинета пользователя
      */
     public function lk()
     {
         if (isset($_SESSION['user']['id'])) {
+            $name = '';
+            $email = '';
+            $subscription = '';
+            $aboutMe = '';
+            $errors = false;
 
-            return new LkView('lk', ['title' => Menu::showTitle(Menu::getUserMenu())]); // Вывод представления
+            if (isset($_POST['submit'])) { // Обработка формы ЛК
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $aboutMe = $_POST['aboutMe'];
+
+                // Проверка ошибок ввода
+                if (!Users::checkName($name)) {
+                    $errors['checkName'] = '- не должно быть короче 2-х символов';
+                }
+
+                if (!($name == $_SESSION['user']['name'])) { // Если имя было изменено
+                    if (Users::checkNameExists($name)) {
+                        $errors['checkNameExists'] = '- такое имя уже используется';
+                    }
+                }
+
+                if (!Users::checkEmail($email)) { //  Проверка правильности ввода e-mail
+                    $errors['checkEmail'] = '- неправильный email';
+                }
+
+                if (!($email == $_SESSION['user']['email'])) { // Если email был изменен
+                    if (Users::checkEmailExists($email)) {
+                        $errors['checkEmailExists'] = '- такой email уже используется';
+                    }
+                }
+
+                if ($_FILES['myfile']['name'] != '') { // Проверка на наличие файла для загрузки
+                    $types = include(CONFIG_DIR . IMAGE_TYPES);
+                    $fileError = SimpleImage::imageFileValidation($types, FILE_SIZE, $_FILES); // Валидация файла изображения
+
+                    if ($fileError) {
+                        $errors['file'] = $fileError; // Если валидация не прошла, то добавляем её ошибки
+                    }
+
+                    if ($errors === false) { // Загружаем файл на сервер
+
+                        if ((DEFAULT_AVATAR != $_SESSION['user']['avatar']) // Если это не заставка 
+                            // && ($name != $_SESSION['user']['name']) // и было изменено имя пользователя,
+                            && file_exists(AVATAR_STORAGE . $_SESSION['user']['avatar'])
+                        ) {
+                            unlink(AVATAR_STORAGE . $_SESSION['user']['avatar']); // то удаляем старый аватар на сервере 
+                        }
+
+                        $myfile = new SplFileInfo($_FILES['myfile']['name']); // Загружаемое имя файла с расширением
+                        $fileName = $name ? $name : $_SESSION['user']['name']; // Имя файла без расширения: новое, если было изменено, иначе - старое
+                        $fileName = $fileName . '.' . $myfile->getExtension(); // Имя файла с расширением
+                        // $fileName = $name . '.' . $myfile->getExtension(); // Имя файла с расширением
+                        $fileMoved = move_uploaded_file($_FILES['myfile']['tmp_name'], AVATAR_STORAGE . $fileName); // Загрузка файла на сервер
+
+                        if (!$fileMoved) {
+                            $errors['file']['LoadServerError'] = 'Файл ' . $fileName . ' не был загружен на сервер';
+                        }
+                    }
+                }
+
+                if ($errors === false) { // Если ошибок нет, то обновляем данные пользователя.
+                    if (Users::updateUser($_SESSION['user']['id'], $name, $email, $aboutMe, ($_FILES['myfile']['name'] != '') ? $fileName : $_SESSION['user']['avatar'])) { // Если обновление прошло нормально
+
+                        // Получаем обновленные данные пользователя
+                        $user = Users::getUserById($_SESSION['user']['id']);
+
+                        if (!$user) {
+                            // Если данные не получены - показываем ошибку
+                            $errors[] = 'Ошибка получения данных.';
+                        } else {
+                            // Если данные правильные, запоминаем пользователя в сессии
+                            Users::auth($user);
+
+                            // Перегружаем кабинет с новыми данными
+                            header('Location: /lk');
+                        }
+                    } else {
+                        $errors[] = 'Ошибка обновления данных.';
+                    }
+                }
+            }
+
+            if (isset($_POST['subscription'])) { // Подписка/Отписка на рассылку
+                $subscription = (int) $_POST['subscription'] ?? 0;
+
+                if (!(in_array($subscription, [0, 1]))) {
+                    $errors[] = 'Некорректные данные. Обратитесь к администртору!';
+                }
+
+                if ($errors === false) { // Если ошибок нет, то обновляем данные пользователя.
+                    if (Users::changeSubscription($_SESSION['user']['id'], $subscription)) { // Если обновление прошло нормально
+                        // Получаем обновленные данные пользователя
+                        $user = Users::getUserById($_SESSION['user']['id']);
+
+                        if (!$user) {
+                            // Если данные не получены - показываем ошибку
+                            $errors[] = 'Ошибка получения данных.';
+                        } else {
+                            // Если данные правильные, запоминаем пользователя в сессии
+                            Users::auth($user);
+
+                            // Перегружаем кабинет с новыми данными
+                            header('Location: /lk');
+
+                            return new View('lk', ['title' => Menu::showTitle(Menu::getUserMenu())]); // Вывод представления
+                        }
+                    } else {
+                        $errors[] = 'Ошибка обновления данных.';
+                    }
+                }
+            }
+
+            return new View(
+                'lk',
+                [
+                    'title' => Menu::showTitle(Menu::getUserMenu()),
+                    'roles' => Roles::all(), // Роли пользователей
+                    'name' =>  $name ?? '',
+                    'email' =>  $email ?? '',
+                    'subscription' => $subscription ?? '',
+                    'aboutMe' => $aboutMe ?? '',
+                    'errors' =>  $errors ?? '',
+
+                ]
+            ); // Вывод представления
         } else {
             header('Location: /login');
         }
--- view/login.php
+++ view/login.php
@@ -9,7 +9,7 @@ include 'layout/header.php';
       <?php if (isset($errors) && is_array($errors)) : ?>
         <ul>
           <?php foreach ($errors as $error) : ?>
-            <li class="font-error"> <?php echo $error; ?></li>
+            <li class="font-error"> <?= $error ?></li>
           <?php endforeach; ?>
         </ul>
       <?php endif; ?>
@@ -19,22 +19,18 @@ include 'layout/header.php';
           <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input type="email" class="form-control
-                              <?php
-                              if (isset($errors)) {
-                                echo "border-error";
-                              }
-                              ?>
-                              " id="email" name="email" placeholder="name@example.com" value="<?php echo $email; ?>">
+                              <?php if ($errors['email'] || $errors['wrongData']) : ?>
+                                border-error
+                              <?php endif ?>
+                              " id="email" name="email" placeholder="name@example.com" value="<?= $email ?>">
           </div>
           <div class="mb-3">
             <label for="password" class="form-label">Пароль</label>
             <input type="password" class="form-control
-                              <?php
-                              if (isset($errors)) {
-                                echo "border-error";
-                              }
-                              ?>
-                              " id="password" name="password" placeholder="password" value="<?php echo $password; ?>">
+                              <?php if ($errors['wrongData']) : ?>
+                                border-error
+                              <?php endif ?>
+                              " id="password" name="password" placeholder="password" value="<?= $password ?>">
           </div>
           <div class="mb-3">
             <button class="btn btn-outline-primary" type="submit" name="submit" id="button-addon1">Войти</button>
